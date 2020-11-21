e1df50c2e4123852cc51099cd8da00c3e24277c7
Andy212 <xfintx@tut.by>
2020-11-21 14:25:24 +0200

    Delete script.js

--- script/script.js
+++
@@ -1,9 +0,0 @@
-let num = 266219;
-const getMultiple = (num) => num.toString().split('').reduce((res, item) => item * res, 1)
-console.log(getMultiple(num));
-
-let pow = getMultiple(num);
-pow **= 3;
-console.log(pow);
-
-console.log(String(pow).slice(0,2));
